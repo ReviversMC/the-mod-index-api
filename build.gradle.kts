c993ec7520ef662dbe0101d64e59aff14aa26fde
Cloudate9 <thealtfulthing@tutanota.com>
2022-04-18 17:19:02 -0700

    I change my mind. Use Java instead of Kotlin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,18 @@
 plugins {
     id ("com.github.johnrengelman.shadow") version "7.1.2"
-    kotlin("jvm") version "1.6.20"
-    kotlin("plugin.serialization") version "1.6.20"
+    java
 }
 
 group = "com.github.reviversmc.themodindex.api"
-version = "1.0.0"
+version = "1.0.0+1.0.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+    implementation("com.squareup.moshi:moshi:1.13.0")
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
 }
 
 tasks {
@@ -20,8 +20,9 @@ tasks {
         options.release.set(17)
     }
 
-    compileKotlin {
-        kotlinOptions.jvmTarget = "17"
+    java {
+        withJavadocJar()
+        withSourcesJar()
     }
 
     shadowJar {
