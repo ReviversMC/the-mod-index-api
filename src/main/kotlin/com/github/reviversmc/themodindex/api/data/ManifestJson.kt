138416190002819208de97d0b12dd5d04da8e652
Cloudate9 <thealtfulthing@tutanota.com>
2022-05-07 17:17:09 -0700

    Add new field as done in v1.1.0 of the index

--- src/main/kotlin/com/github/reviversmc/themodindex/api/data/ManifestJson.kt
+++ src/main/kotlin/com/github/reviversmc/themodindex/api/data/ManifestJson.kt
@@ -51,9 +51,14 @@ data class ManifestJson(
      * @param mcVersions   A list of Minecraft versions the file is compatible with.
      * @param sha1Hash     The sha1 hash of the file.
      * @param downloadUrls A list of urls to download the file from.
+     * @param curseDownloadAvailable Whether the file is available on Curse. A further api call to CF is required to get the download url.
      */
     @kotlinx.serialization.Serializable
     data class ManifestFile(
-        val fileName: String?, val mcVersions: List<String>, val sha1Hash: String?, val downloadUrls: List<String>
+        val fileName: String?,
+        val mcVersions: List<String>,
+        val sha1Hash: String?,
+        val downloadUrls: List<String>,
+        val curseDownloadAvailable: Boolean?
     )
 }
