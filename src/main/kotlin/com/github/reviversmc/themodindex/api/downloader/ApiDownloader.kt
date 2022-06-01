fefbc13950990409a9cc8fc0a1bccb8b6d10c30a
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-05-31 20:58:24 -0700

    Remove deprecated methods

--- src/main/kotlin/com/github/reviversmc/themodindex/api/downloader/ApiDownloader.kt
+++ src/main/kotlin/com/github/reviversmc/themodindex/api/downloader/ApiDownloader.kt
@@ -17,7 +17,7 @@ interface ApiDownloader {
      * @author ReviversMC
      * @since 1.0.0-2.0.0
      */
-    val repositoryUrlAsString: String
+    val formattedBaseUrl: String
 
     /**
      * The index.json file for this repository. Only retrieves the index.json file locally if it has already been downloaded.
@@ -37,16 +37,6 @@ interface ApiDownloader {
     fun downloadIndexJson(): IndexJson?
 
     /**
-     * The manifest.json file for this repository. Always re-downloads the index.json file. Runs asynchronously.
-     * @return The manifest.json file for this repository.
-     * @author ReviversMC
-     * @since 5.1.0
-     */
-    @Deprecated("This method offers no advantages over #downloadIndexJson. Use that instead. To be removed in next major release (6.0.0)", ReplaceWith("downloadIndexJson()"))
-    @ExperimentalSerializationApi
-    suspend fun asyncDownloadIndexJson(): IndexJson?
-
-    /**
      * The manifest.json file for this repository. Downloads the index.json file if it has not already been downloaded. Runs synchronously.
      * @return The manifest.json file for this repository.
      * @author ReviversMC
@@ -54,16 +44,6 @@ interface ApiDownloader {
      */
     @ExperimentalSerializationApi
     fun getOrDownloadIndexJson(): IndexJson?
-
-    /**
-     * The manifest.json file for this repository. Downloads the index.json file if it has not already been downloaded. Runs asynchronously.
-     * @return The manifest.json file for this repository.
-     * @author ReviversMC
-     * @since 5.1.0
-     */
-    @Deprecated("This method offers no advantages over #getOrDownloadIndexJson. Use that instead. To be removed in next major release (6.0.0)", ReplaceWith("getOrDownloadIndexJson()"))
-    @ExperimentalSerializationApi
-    suspend fun getOrAsyncDownloadIndexJson(): IndexJson?
 
     /**
      * The manifest.json file for this repository. Runs synchronously.
@@ -77,18 +57,6 @@ interface ApiDownloader {
     @ExperimentalSerializationApi
     fun downloadManifestJson(genericIdentifier: String): ManifestJson?
 
-    /**
-     * The manifest.json file for this repository. Runs asynchronously.
-     * @param genericIdentifier The generic identifier is "modLoader:modName".
-     * This is similar to the identifier in [IndexJson.identifiers],
-     * but does not contain the version.
-     * @return The requested manifest.json file.
-     * @author ReviversMC
-     * @since 5.1.0
-     */
-    @Deprecated("This method offers no advantages over #downloadManifestJson. Use that instead. To be removed in next major release (6.0.0)", ReplaceWith("downloadManifestJson(genericIdentifier)"))
-    @ExperimentalSerializationApi
-    suspend fun asyncDownloadManifestJson(genericIdentifier: String): ManifestJson?
 
     /**
      * A [ManifestJson.ManifestFile] for the given identifier. Runs synchronously.
@@ -100,16 +68,4 @@ interface ApiDownloader {
      */
     @ExperimentalSerializationApi
     fun downloadManifestFileEntry(identifier: String): ManifestJson.ManifestFile?
-
-    /**
-     * A [ManifestJson.ManifestFile] for the given identifier. Runs asynchronously.
-     * This is not to be confused with the manifest.json file, which can be obtained with [downloadManifestJson]
-     * @param identifier The identifier of the file entry to retrieve.
-     * @return The requested manifest.json file.
-     * @author ReviversMC
-     * @since 5.1.0
-     */
-    @Deprecated("This method offers no advantages over #downloadManifestFileEntry. Use that instead. To be removed in next major release (6.0.0)", ReplaceWith("downloadManifestFileEntry(identifier)"))
-    @ExperimentalSerializationApi
-    suspend fun asyncDownloadManifestFileEntry(identifier: String): ManifestJson.ManifestFile?
 }
